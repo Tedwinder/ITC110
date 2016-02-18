860cfac376141238b809f3149b28838b8e1cd3ad
Tedwinder <TedCallow76@gmail.com>
2016-02-17 21:21:11 -0800

    Update TablePrinter.py

--- TablePrinter.py
+++ TablePrinter.py
@@ -1,18 +1,20 @@
 
-
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 def tablePrinter(tableData):
-    i = 0;             
-    while (len(str(tableData[i])) > 0):
-
-        i += 1
-  
-        if (i%3==0):
-            print(str(tableData[i]) + "\n");
-        else:
-            print(str(tableData[i]) + "\t"); 
-
+    i = 0;
+    
+    for i in range(0,len(tableData)):
+        #colWidths = [i] * len(tableData);
+        p=0;
+        for p in range(0,len(tableData[i])):
+            if ((p/3).is_integer() and p!=0):
+                print((str(tableData[i][p])).rjust(5), end="\n");
+            else:
+                print((str(tableData[i][p])).rjust(5), end="\t\t"); 
+        
+        
 tablePrinter(tableData);
+
